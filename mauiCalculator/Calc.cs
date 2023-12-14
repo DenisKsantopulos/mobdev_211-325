0b06ded2ddc0412994ba93950fa8b118369cf380
DenisKsantopulos <denksant@gmail.com>
2023-12-14 15:55:12 +0300

    14/12

---
+++ mauiCalculator/Calc.cs
@@ -0,0 +1,34 @@
+﻿namespace mauiCalculator
+{
+    internal class Calc
+    {
+        private double firstNumber;
+
+        public string CurText { get; private set; }
+        public string CurOper { get; private set; }
+
+        public event EventHandler Changed;
+
+        internal void Clear()
+        {
+            CurText = "";
+            Changed?.Invoke(this, EventArgs.Empty);
+        }
+
+        internal void PressNum(int v)
+        {
+            CurText += v.ToString();
+            Changed?.Invoke(this, EventArgs.Empty);
+        }
+
+        internal void PressOperator(string oper)
+        {
+            if (firstNumber != 0)
+                    {
+                //
+            }
+            double.TryParse(CurText, out firstNumber);
+            CurOper = oper;
+        }
+    }
+}
---
+++ mauiCalculator/MainPage.xaml.cs
@@ -0,0 +1,30 @@
+﻿namespace mauiCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        Calc calc = new(); 
+        public MainPage()
+        {
+            InitializeComponent();
+
+            calc.Changed += (s, e) => laDisplay.Text = calc.CurText;
+
+            buCE.Clicked += (s, e) => calc.Clear();
+            buNum0.Clicked += (s, e) => calc.PressNum(0);
+            buNum1.Clicked += (s, e) => calc.PressNum(1);
+            buNum2.Clicked += (s, e) => calc.PressNum(2);
+            buNum3.Clicked += (s, e) => calc.PressNum(3);
+            buNum4.Clicked += (s, e) => calc.PressNum(4);
+            buNum5.Clicked += (s, e) => calc.PressNum(5);
+            buNum6.Clicked += (s, e) => calc.PressNum(6);
+            buNum7.Clicked += (s, e) => calc.PressNum(7);
+            buNum8.Clicked += (s, e) => calc.PressNum(8);
+            buDiv.Clicked += (s, e) => calc.PressOperator("div");
+            buSum.Clicked += (s, e) => calc.PressOperator("sum");
+            buMul.Clicked += (s, e) => calc.PressOperator("mul");
+            buSub.Clicked += (s, e) => calc.PressOperator("sub");
+        }
+
+        
+    }
+}
